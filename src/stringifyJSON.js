035f511768e26f5b01abd30f0e5b46eebbb3d450
ttreit <14933277+ttreit@users.noreply.github.com>
2020-01-27 13:30:18 -0800

    Create initial structure

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -1,8 +1,58 @@
-// this is what you would do if you liked things to be easy:
-// var stringifyJSON = JSON.stringify;
-
-// but you don't so you're going to write it from scratch:
-
 var stringifyJSON = function(obj) {
-  // your code goes here
+  let leftside = '{';
+  let rightside = '}';
+  let middle = '';
+
+  //functions for parsing
+  function stringArray() {
+
+  }
+
+  function stringObject() {
+
+  }
+
+  function stringOther() {
+
+  }
+
+
+
+
+
+  return `${leftside}${middle}${rightside}`;
 };
+
+
+
+
+// Test Data
+let simpleTestData = {
+  'a' : 123,
+  'b' : 'Tammy',
+  'c' : [1, 2, 3],
+  'd' : {'z' : 123, 'y': 'abc'}
+};
+
+{"a":123,"b":"Tammy","c":[1,2,3],"d":{"z":123,"y":"abc"}}
+
+//three main things to solve:
+// 1. arrays
+// 2. objects
+// 3. everything else
+
+
+// Simple Test
+let theirs = JSON.stringify(simpleTestData);
+console.log(theirs);
+console.log ('-----------------------')
+let mine = stringifyJSON(simpleTestData);
+console.log (mine);
+
+if (theirs === mine) {
+  console.log('PASSED!');
+} else {
+  console.log('FAILED');
+}
+
+
