f81a2ba19e7308f83966a5a115f564a90fe11c41
ttreit <14933277+ttreit@users.noreply.github.com>
2020-01-27 15:55:24 -0800

    Add scaffolding for functions

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -3,41 +3,67 @@ var stringifyJSON = function(obj) {
   let rightside = '}';
   let middle = '';
 
+
+
+  //Is Dictionary function
+  //From StackOverflow
+  //https://stackoverflow.com/questions/38304401/javascript-check-if-dictionary
+  function isDict(v) {
+      return typeof v==='object' && v!==null && !(v instanceof Array) && !(v instanceof Date);
+  }
+
   //sorting types to be turned to strings
 
-  function sortTypes() {
+  function iterateObject() {     //TODO rename function
+      // iterate over object
+      //get k/v pair
+      //key is already a string
+      //append key to 'middle'
+        //key should be in quotes
+      //append :
+      //append value as string
+        //send to be sorted
+        //receive text string from sortValueTypes
+  }
+
+
+  function sortValueTypes() {
+      //test value for type
+      //send value to correct parsing function
+      //return text string
 
   }
 
-  //functions for parsing
+  //functions for parsing values
   function stringArray() {
       let arrayResult;
-        //test
-        arrayResult = 'array';
+
       return middle = middle + arrayResult;
   }
 
   function stringObject() {
       let objectResult;
-       //test
-       objectResult = 'object';
+
       return middle = middle + objectResult;
 
   }
 
   function stringOther() {
       let otherResult;
-      //test
-      otherResult = 'other';
+
       return middle = middle + otherResult;
 
   }
 
 
+  //********Main**********
+  //test obj if not a dictionary return obj as is.
+  if (!isDict(obj)) {
+      return obj;
+  }
 
-  stringArray();
-  stringObject();
-  stringOther();
+
+
   return `${leftside}${middle}${rightside}`;
 };
 
@@ -52,13 +78,9 @@ let simpleTestData = {
   'd' : {'z' : 123, 'y': 'abc'}
 };
 
+let otherTestData = {'a' : 1};
+
 //{"a":123,"b":"Tammy","c":[1,2,3],"d":{"z":123,"y":"abc"}}
-
-//three main things to solve:
-// 1. arrays
-// 2. objects
-// 3. everything else
-
 
 // Simple Test
 let theirs = JSON.stringify(simpleTestData);
@@ -72,5 +94,10 @@ if (theirs === mine) {
 } else {
   console.log('FAILED');
 }
+
+let otherTest = JSON.stringify(otherTestData);
+console.log('their otherTest:', otherTest);
+let myOtherTest = stringifyJSON(otherTestData);
+console.log(myOtherTest);
 
 
